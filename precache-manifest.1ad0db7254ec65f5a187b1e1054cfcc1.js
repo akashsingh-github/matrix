56f5797dd993f31dea73646a8287810eca4f51b7
Akash singh <robinakash123@gmail.com>
2021-12-04 17:27:06 +0530

    Updates

---
+++ precache-manifest.1ad0db7254ec65f5a187b1e1054cfcc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de626eb8f312a6ab84275180109b272",
+    "url": "/matrix/index.html"
+  },
+  {
+    "revision": "be4429412af326eb11fa",
+    "url": "/matrix/static/css/main.10aec758.chunk.css"
+  },
+  {
+    "revision": "c3157a7c9065edb2e8dc",
+    "url": "/matrix/static/js/2.6480014f.chunk.js"
+  },
+  {
+    "revision": "459933aebd8dd6902bca79a6c18dabee",
+    "url": "/matrix/static/js/2.6480014f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4429412af326eb11fa",
+    "url": "/matrix/static/js/main.773ff20e.chunk.js"
+  },
+  {
+    "revision": "3ed535e09c9e7e3ff379",
+    "url": "/matrix/static/js/runtime-main.37b5eafc.js"
+  }
+]);
